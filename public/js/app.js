ee90fa6760b03e6d65c29d3fb7f4682a592ee8ba
SheiShei <s.cmanawit@gmail.com>
2018-12-01 23:20:35 +0800

    :|

--- public/js/app.js
+++ public/js/app.js
@@ -19704,8 +19704,8 @@ window.Pusher = __webpack_require__(42);
 
 window.Echo = new __WEBPACK_IMPORTED_MODULE_0_laravel_echo___default.a({
     broadcaster: 'pusher',
-    key: "",
-    cluster: "mt1",
+    key: "22c7b2a3c635de52dc73",
+    cluster: "ap1",
     encrypted: true
 });
 
@@ -59377,6 +59377,10 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 
         };
     },
+    created: function created() {
+        var data = this.data;
+        this.$store.dispatch('setUsers', { url: '/api/shittycaptivateusers', data: data });
+    },
 
 
     computed: _extends({}, Object(__WEBPACK_IMPORTED_MODULE_0_vuex__["b" /* mapGetters */])({
@@ -59799,90 +59803,92 @@ var render = function() {
             ])
           ]),
           _vm._v(" "),
-          _c(
-            "users-table",
-            { attrs: { data: _vm.data } },
-            _vm._l(_vm.users, function(user) {
-              return _c("tr", { key: user.id }, [
-                _c("td", { staticClass: "text-center" }, [
-                  _vm._v(_vm._s(user.id))
-                ]),
-                _vm._v(" "),
-                _c("td", [_vm._v(_vm._s(user.name))]),
-                _vm._v(" "),
-                _c("td", [_vm._v(_vm._s(user.email))]),
-                _vm._v(" "),
-                _c("td", [_vm._v(_vm._s(user.role.name))]),
-                _vm._v(" "),
-                _c("td", [_vm._v(_vm._s(user.created_at))]),
-                _vm._v(" "),
-                _c("td", { staticClass: "td-actions text-right" }, [
-                  _vm.data.notArchive
-                    ? _c(
-                        "button",
-                        {
-                          staticClass: "btn btn-success btn-simple btn-xs",
-                          attrs: {
-                            type: "button",
-                            rel: "tooltip",
-                            "data-original-title": "",
-                            title: "Edit"
-                          },
-                          on: {
-                            click: function($event) {
-                              _vm.updateUser(user)
-                            }
-                          }
-                        },
-                        [_c("i", { staticClass: "fa fa-edit" })]
-                      )
-                    : _vm._e(),
-                  _vm._v(" "),
-                  _vm.data.notArchive
-                    ? _c(
-                        "button",
-                        {
-                          staticClass: "btn btn-danger btn-simple btn-xs",
-                          attrs: {
-                            type: "button",
-                            rel: "tooltip",
-                            "data-original-title": "",
-                            title: "Archive"
-                          },
-                          on: {
-                            click: function($event) {
-                              _vm.deleteUser(user.id)
-                            }
-                          }
-                        },
-                        [_c("i", { staticClass: "fa fa-trash-o" })]
-                      )
-                    : _vm._e(),
-                  _vm._v(" "),
-                  !_vm.data.notArchive
-                    ? _c(
-                        "button",
-                        {
-                          staticClass: "btn btn-danger btn-simple btn-xs",
-                          attrs: {
-                            type: "button",
-                            rel: "tooltip",
-                            "data-original-title": "",
-                            title: "Restore"
-                          },
-                          on: {
-                            click: function($event) {
-                              _vm.restoreUser(user.id)
-                            }
-                          }
-                        },
-                        [_c("i", { staticClass: "fa fa-refresh" })]
-                      )
-                    : _vm._e()
-                ])
-              ])
-            })
-          ),
+          _vm.users
+            ? _c(
+                "users-table",
+                { attrs: { data: _vm.data } },
+                _vm._l(_vm.users, function(user) {
+                  return _c("tr", { key: user.id }, [
+                    _c("td", { staticClass: "text-center" }, [
+                      _vm._v(_vm._s(user.id))
+                    ]),
+                    _vm._v(" "),
+                    _c("td", [_vm._v(_vm._s(user.name))]),
+                    _vm._v(" "),
+                    _c("td", [_vm._v(_vm._s(user.email))]),
+                    _vm._v(" "),
+                    _c("td", [_vm._v(_vm._s(user.role.name))]),
+                    _vm._v(" "),
+                    _c("td", [_vm._v(_vm._s(user.created_at))]),
+                    _vm._v(" "),
+                    _c("td", { staticClass: "td-actions text-right" }, [
+                      _vm.data.notArchive
+                        ? _c(
+                            "button",
+                            {
+                              staticClass: "btn btn-success btn-simple btn-xs",
+                              attrs: {
+                                type: "button",
+                                rel: "tooltip",
+                                "data-original-title": "",
+                                title: "Edit"
+                              },
+                              on: {
+                                click: function($event) {
+                                  _vm.updateUser(user)
+                                }
+                              }
+                            },
+                            [_c("i", { staticClass: "fa fa-edit" })]
+                          )
+                        : _vm._e(),
+                      _vm._v(" "),
+                      _vm.data.notArchive
+                        ? _c(
+                            "button",
+                            {
+                              staticClass: "btn btn-danger btn-simple btn-xs",
+                              attrs: {
+                                type: "button",
+                                rel: "tooltip",
+                                "data-original-title": "",
+                                title: "Archive"
+                              },
+                              on: {
+                                click: function($event) {
+                                  _vm.deleteUser(user.id)
+                                }
+                              }
+                            },
+                            [_c("i", { staticClass: "fa fa-trash-o" })]
+                          )
+                        : _vm._e(),
+                      _vm._v(" "),
+                      !_vm.data.notArchive
+                        ? _c(
+                            "button",
+                            {
+                              staticClass: "btn btn-danger btn-simple btn-xs",
+                              attrs: {
+                                type: "button",
+                                rel: "tooltip",
+                                "data-original-title": "",
+                                title: "Restore"
+                              },
+                              on: {
+                                click: function($event) {
+                                  _vm.restoreUser(user.id)
+                                }
+                              }
+                            },
+                            [_c("i", { staticClass: "fa fa-refresh" })]
+                          )
+                        : _vm._e()
+                    ])
+                  ])
+                })
+              )
+            : _vm._e(),
           _vm._v(" "),
           _c("div", { staticClass: "mybox-footer userlist-ft" })
         ],
